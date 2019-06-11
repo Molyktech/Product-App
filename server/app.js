74cc95f35c9036c96a5dbcf2239f65a97c000479
Modupe Adebayo <modupeadebayo001@gmail.com>
2019-06-11 02:24:01 +0100

    routes

--- server/app.js
+++ server/app.js
@@ -5,6 +5,8 @@ const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const cors = require("cors");
 
+const product = require("./api/productRoute");
+
 const app = express();
 
 app.use(logger("dev"));
@@ -18,6 +20,9 @@ app.get("/", (req, res) => {
     message: "Hello World"
   });
 });
+
+app.use("/api/v1/products", product);
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
