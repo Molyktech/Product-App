d4f50a80a5c05b15fe7c1946c1b80f464a5bc39f
Modupe Adebayo <modupeadebayo001@gmail.com>
2019-06-11 13:50:46 +0100

    get single product from server side

--- server/api/productRoute.js
+++ server/api/productRoute.js
@@ -11,4 +11,23 @@ router.get("/", (req, res) => {
   });
 });
 
+router.get("/:id", (req, res, next) => {
+  //validate the id by checking if it is a number
+  if (!isNaN(req.params.id)) {
+    //find it in the db
+    // res.json("hello");
+    queries.getOne(req.params.id).then(product => {
+      if (product) {
+        res.json(product);
+      } else {
+        next(); //next without error code cals the next available error code
+      }
+    });
+  } else {
+    const error = new Error("Invalid ID");
+    //SEND IT TO THE ERRO HANDLER
+    next(error);
+  }
+});
+
 module.exports = router;
--- server/db/queries.js
+++ server/db/queries.js
@@ -1,9 +1,14 @@
 const connection = require("./connection");
 
 module.exports = {
-  getAll() {
+  getAll(id) {
     //return connection with product name
     //same thing as select * from product
     return connection("product");
+  },
+  getOne(id) {
+    return connection("product")
+      .where("id", id)
+      .first();
   }
 };
